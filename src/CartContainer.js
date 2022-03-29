ef33e76705b6dd661179b4140845f6c9c7deb2d5
ashish kachhadiya <ashishkachhadiya22@gmail.com>
2022-03-29 17:05:18 +0530

    final commit

---
+++ src/CartContainer.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { MDBBtn, MDBIcon } from "mdb-react-ui-kit";
+import CartItem from "./CartItem";
+import {
+  clearCart,
+  getCartTotal,
+  getCartItems,
+} from "./redux/feature/cartSlice";
+
+const CartContainer = () => {
+  const { items, totalAmount } = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCartTotal());
+  }, [items]);
+  if (items.length === 0) {
+    return (
+      <>
+        <h3 className="fs-bold mt-4"> Your Shopping Cart is Empty </h3>
+        <MDBBtn className="mx-2 " onClick={() => dispatch(getCartItems())}>
+          {" "}
+          getItems
+        </MDBBtn>
+      </>
+    );
+  }
+  return (
+    <>
+      <h2 className="lead-mb-0 mt-2">Your Shopping Cart</h2>
+      {items.map(item => {
+        return <CartItem key={item.id} {...item} />;
+      })}
+      <footer>
+        <hr />
+        <div>
+          <h4
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              marginRight: "60px",
+            }}
+          >
+            Total <span>${totalAmount}</span>
+          </h4>
+        </div>
+        <MDBBtn
+          color="danger"
+          style={{ width: "140px", marginTop: "40px" }}
+          onClick={() => dispatch(clearCart())}
+        >
+          Clear Cart
+        </MDBBtn>
+      </footer>
+    </>
+  );
+};
+
+export default CartContainer;
